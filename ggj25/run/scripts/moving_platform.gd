a2b4a427abaff612a1151a4513d022ec17216736
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2025-01-25 19:16:09 -0500

    added looping to moving platforms

--- ggj25/run/scripts/moving_platform.gd
+++ ggj25/run/scripts/moving_platform.gd
@@ -5,6 +5,7 @@ class_name MovingPlatform
 @export var path_time = 1.0
 @export var ease: Tween.EaseType
 @export var transition: Tween.TransitionType
+@export var looping = false
 
 
 func _ready() -> void:
@@ -14,5 +15,8 @@ func _ready() -> void:
 func _move_tween():
 	var tween = get_tree().create_tween().set_loops()
 	tween.tween_property(path_follow, "progress_ratio", 1.0, path_time).set_ease(ease).set_trans(transition)
-	tween.tween_property(path_follow, "progress_ratio", 0.0, path_time).set_ease(ease).set_trans(transition)
+	if not looping:
+		tween.tween_property(path_follow, "progress_ratio", 0.0, path_time).set_ease(ease).set_trans(transition)
+	else:
+		tween.tween_property(path_follow, "progress_ratio", 0.0, 0.0).set_ease(ease).set_trans(transition)
 	
