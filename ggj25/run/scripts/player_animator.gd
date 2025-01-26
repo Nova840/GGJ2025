bdfca17446937770f39db50f60dd3adf248b29de
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2025-01-25 20:55:01 -0500

    added bubble animation; minor refactor; fixed gamemanager call

--- ggj25/run/scripts/player_animator.gd
+++ ggj25/run/scripts/player_animator.gd
@@ -5,11 +5,10 @@ class_name PlayerAnimator
 @export var sprite: Sprite2D
 
 func _process(delta: float) -> void:
-	# TODO - on move rotate sprite
 	if player_controller.direction == 1:
-		sprite.flip_h = false
+		sprite.rotation_degrees += 10
 	elif player_controller.direction == -1:
-		sprite.flip_h = true
+		sprite.rotation_degrees -= 10
 	
 	if not player_controller.is_alive or player_controller.finished_level:
 		sprite.visible = false
